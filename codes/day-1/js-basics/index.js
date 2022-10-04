ab79f04048c6c3a16f75820ba73fd285d87ad8a1
Joydip <joydip.mondal@gmail.com>
2022-10-04 11:49:57 +0530

    filter code uploaded

---
+++ codes/day-1/js-basics/index.js
@@ -0,0 +1,9 @@
+function filterValues(arr) {
+
+}
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+const result = filterValues(numbers)
+for (let index = 0; index < result.length; index++) {
+    const element = result[index];
+    console.log(element)
+}
---
+++ codes/day-1/js-basics/playground/array-basics.js
@@ -0,0 +1,10 @@
+const x = 10
+//x = 20
+console.log(x)
+
+//const arr = [1, 2, 3, 4]
+const arr = new Array(1, 2, 3, 4)
+//arr = []
+arr.push(10, 20, 30, 40)
+arr.splice(2, 3, 300, 400, 100)
+console.log(arr)
