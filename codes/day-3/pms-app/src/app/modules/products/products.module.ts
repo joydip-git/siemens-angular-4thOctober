a51f657aadaa6d9f82622bcbc37c10d90a9980cc
Joydip <joydip.mondal@gmail.com>
2022-10-07 13:03:49 +0530

    service updated

--- codes/day-3/pms-app/src/app/modules/products/products.module.ts
+++ codes/day-3/pms-app/src/app/modules/products/products.module.ts
@@ -4,12 +4,13 @@ import { FilterProductPipe } from "./pipes/filter-product.pipe";
 import { ProductListComponent } from "./components/product-list/product-list.component";
 import { FilterComponent } from './components/filter/filter.component';
 import { StarComponent } from './components/star/star.component';
-import { ProductService } from "./services/product.service";
+//import { ProductService } from "./services/product.service";
 import { PRODUCT_SERVICE, PRODUCT_SERVICE_CLASS } from "src/constants/app.constants";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
     declarations: [ProductListComponent, FilterProductPipe, FilterComponent, StarComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, HttpClientModule],
     //providers: [ProductService],
     providers: [
         {
--- codes/day-3/pms-app/src/app/modules/products/services/product.service.ts
+++ codes/day-3/pms-app/src/app/modules/products/services/product.service.ts
@@ -1,10 +1,18 @@
 import { products } from "../../../../data/products";
 import { Product } from "../models/product.model";
 import { IProductService } from "../models/productservice.contract";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
+// @Injectable({
+//     providedIn: 'root'
+// })
+@Injectable()
 export class ProductService implements IProductService {
+    constructor(private http: HttpClient) {
+
+    }
     getProducts(): Product[] {
         return products
     }
-
 }
